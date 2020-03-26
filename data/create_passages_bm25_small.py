4f25eb46858d4c426e4173211855447ae0afe15f
remi calizzano <remi.calizzano@gmail.com>
2020-03-26 10:27:54 +0100

    Optimize small passages creating function

--- data/create_passages_bm25_small.py
+++ data/create_passages_bm25_small.py
@@ -1,20 +1,50 @@
-import pandas as pd
 import json
-import argparse
-
 from tqdm import tqdm
+import argparse
+import pandas as pd
 
+
+'''
+Convert collections json docs to a dict {pid: passage}
+'''
+docs_path = ["collections/docs00.json",
+             "collections/docs01.json",
+             "collections/docs02.json",
+             "collections/docs03.json",
+             "collections/docs04.json",
+             "collections/docs05.json",
+             "collections/docs06.json",
+             "collections/docs07.json",
+             "collections/docs08.json"]
+
+passages = {}
+for doc_path in docs_path:
+    with open(doc_path, 'r') as f:
+        lines = f.readlines()
+        print(
+            "Number of lines: {} for {}".format(len(lines), doc_path)
+        )
+        for line in tqdm(lines, desc="convert lines to dict"):
+            passage = json.loads(line)
+            passages[passage['id']] = passage['contents']
+            
+
+print(
+    "Passages | type: {}, length: {}".format(type(passages), len(passages))
+)
+
+
+'''
+Find pids list to read
+'''
 parser = argparse.ArgumentParser()
 parser.add_argument("n_top", type=int, help="number of passages to re-rank after BM25")
 args = parser.parse_args()
 
 
 bm25_run_path = "evaluation/bm25/run.dev.small.tsv"
-docs_paths = ["collections/docs00.json", "collections/docs01.json", "collections/docs02.json", "collections/docs03.json",
-              "collections/docs04.json", "collections/docs05.json", "collections/docs06.json", "collections/docs07.json",
-              "collections/docs08.json"]
 
-bm25_df = pd.read_csv(bm25_run_path, sep='	', header=0, names=['qid', 'pid', 'rank'])
+bm25_df = pd.read_csv(bm25_run_path, sep='	', header=None, names=['qid', 'pid', 'rank'])
 bm25_df = bm25_df.loc[bm25_df['rank'] <= args.n_top]
 
 qids = list(set(bm25_df['qid'].to_list()))
@@ -25,19 +55,17 @@ print('Number of qids: ', len(qids))
 print('Number of pids: ', len(pids))
 print()
 
-small_pids = {}
-for i, doc in enumerate(docs_paths):
-    with open(doc, "r") as f:
-        for p in tqdm(f.readlines(), desc='document '+str(i)+' processing'):
-            passage = json.loads(p)
-            if int(passage['id']) in pids:
-                small_pids[passage['id']] = passage['contents']
 
-print('Number of passage found: ', len(small_pids))
+'''
+Create passages small json file
+'''
+small_passages = {}
+for pid in tqdm(pids, desc="Creating small passages"):
+    small_passages[pid] = passages[str(pid)]
+
+print('Number of passage found: ', len(small_passages))
 print()
 
-with open('passages.bm25.small.json', 'w') as fp:
-    json.dump(small_pids, fp)
+with open('passages/passages.bm25.small.json', 'w') as fp:
+    json.dump(small_passages, fp)
         
-
-
--- data/create_passages_test_small.py
+++ data/create_passages_test_small.py
@@ -1,12 +1,42 @@
-import pandas as pd
 import json
-
 from tqdm import tqdm
+import pandas as pd
 
+
+'''
+Convert collections json docs to a dict {pid: passage}
+'''
+docs_path = ["collections/docs00.json",
+             "collections/docs01.json",
+             "collections/docs02.json",
+             "collections/docs03.json",
+             "collections/docs04.json",
+             "collections/docs05.json",
+             "collections/docs06.json",
+             "collections/docs07.json",
+             "collections/docs08.json"]
+
+passages = {}
+for doc_path in docs_path:
+    with open(doc_path, 'r') as f:
+        lines = f.readlines()
+        print(
+            "Number of lines: {} for {}".format(len(lines), doc_path)
+        )
+        for line in tqdm(lines, desc="convert lines to dict"):
+            passage = json.loads(line)
+            passages[passage['id']] = passage['contents']
+            
+
+print(
+    "Passages | type: {}, length: {}".format(type(passages), len(passages))
+)
+
+
+'''
+Find pids list to read
+'''
 qrels_test_path = "qrels/qrels.test.tsv"
-docs_paths = ["collections/docs00.json", "collections/docs01.json", "collections/docs02.json", "collections/docs03.json",
-              "collections/docs04.json", "collections/docs05.json", "collections/docs06.json", "collections/docs07.json",
-              "collections/docs08.json"]
 
 qrels_df = pd.read_csv(qrels_test_path, sep=' ', header=None, names=['qid', 'Q0', 'pid', 'rating'])
 
@@ -23,19 +53,18 @@ print('Number of rate 2: ', rating.count(2))
 print('Number of rate 3: ', rating.count(3))
 print()
 
-small_pids = {}
-for i, doc in enumerate(docs_paths):
-    with open(doc, "r") as f:
-        for p in tqdm(f.readlines(), desc='document '+str(i)+' processing'):
-            passage = json.loads(p)
-            if int(passage['id']) in pids:
-                small_pids[passage['id']] = passage['contents']
+'''
+Create passages small json file
+'''
+small_passages = {}
+for pid in tqdm(pids, desc="Creating small passages"):
+    small_passages[pid] = passages[str(pid)]
 
-print('Number of passage found: ', len(small_pids))
+print('Number of passage found: ', len(small_passages))
 print()
 
 with open('passages.test.small.json', 'w') as fp:
-    json.dump(small_pids, fp)
+    json.dump(small_passages, fp)
         
 
 
