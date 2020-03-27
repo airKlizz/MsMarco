ca5569d2d3a1b031984c573779e953a438b4fab4
remi calizzano <remi.calizzano@gmail.com>
2020-03-27 12:33:26 +0100

    Debug scorer output to solve loss issue

--- train.py
+++ train.py
@@ -34,7 +34,7 @@ def train_step(model, optimizer, loss, inputs, gold, train_loss, train_acc):
     with tf.GradientTape() as tape:
         predictions = model(inputs, training=True)
         loss = loss(gold, predictions)
-        print("Gold: {} Predictions: {} Loss: {} Acc: {}".format(gold.eval(), predictions.eval(), loss.eval(), ScoreAccuracy.calculate_score_accuracy(gold, predictions, 1/6).eval()))
+    print("Gold: {} Predictions: {} Loss: {} Acc: {}".format(gold.eval(), predictions.eval(), loss.eval(), ScoreAccuracy.calculate_score_accuracy(gold, predictions, 1/6).eval()))
     gradients = tape.gradient(loss, model.trainable_variables)
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
     train_loss(loss)
